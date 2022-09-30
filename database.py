eba398c8c06e2d71dbba00c93fa30c36d25e469e
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2022-09-30 16:06:59 -0400

    Creating insert function to add value in database

--- database.py
+++ database.py
@@ -1,12 +1,28 @@
 """Module providing database tables and operations support."""
-from sqlalchemy import Column, ForeignKey, Integer, Boolean, String, JSON, DateTime
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Integer,
+    Boolean,
+    String,
+    JSON,
+    DateTime
+    )
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relation, sessionmaker
+from sqlalchemy.orm import relationship, sessionmaker
 
 Base = declarative_base()
+# session = sessionmaker()(bind=engine)
 
 class BaseTable(Base):
-    """docstring for BaseTable"""
+    """
+    Define fields common of all tables in database
+    BaseTable:
+        id integer [PK]
+        deleted boolean
+        extra_metadata json
+    """
+    __abstract__ = True
 
     id = Column(Integer, primary_key=True)
     deleted = Column(Boolean, default=False)
@@ -17,10 +33,16 @@ class BaseTable(Base):
         self.extra_metadata = extra_metadata
 
 class Project(BaseTable):
-    """docstring for Project"""
+    """
+    Project:
+        id integer [PK]
+        name text
+        deleted boolean
+        extra_metadata json
+    """
     __tablename__ = "project"
 
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False, unique=True)
 
     def __init__(self, name=None):
         super().__init__()
@@ -30,7 +52,17 @@ class Project(BaseTable):
         return f"Project({self.name!r}, {self.deleted!r}, {self.extra_metadata!r})"
 
 class Patient(BaseTable):
-    """docstring for Patient"""
+    """
+    Patient:
+        id integer [PK]
+        project_id integer [ref: > project.id]
+        name text
+        alias blob
+        cohort text
+        institution text
+        deleted boolean
+        extra_metadata json
+    """
     __tablename__ = "patient"
 
     project_id = Column(Integer, ForeignKey("project.id"))
@@ -39,7 +71,7 @@ class Patient(BaseTable):
     cohort = Column(String, nullable=True)
     institution = Column(String, nullable=True)
 
-    project = relation("Project", backref="patient", lazy=False)
+    project = relationship("Project", backref="patient", lazy=False)
 
     def __init__(self, name=None, alias=None, cohort=None, institution=None):
         super().__init__()
@@ -52,15 +84,23 @@ class Patient(BaseTable):
         return f"Patient({self.project!r}, {self.name!r}, {self.alias!r}, {self.cohort!r}, {self.institution!r}, {self.deleted!r}, {self.extra_metadata!r})"
 
 class Experiment(BaseTable):
-    """docstring for experiment"""
+    """
+    Experiment:
+        id integer [PK]
+        run_id integer [ref: > run.id]
+        project_id integer [ref: > project.id]
+        sequencing_technology text
+        deleted boolean
+        extra_metadata json
+    """
     __tablename__ = "experiment"
 
     run_id = Column(Integer, ForeignKey("run.id"))
     project_id = Column(Integer, ForeignKey("project.id"))
     sequencing_technology = Column(String, nullable=True)
 
-    run = relation("Run", backref="experiment", lazy=False)
-    project = relation("Project", backref="experiment", lazy=False)
+    run = relationship("Run", backref="experiment", lazy=False)
+    project = relationship("Project", backref="experiment", lazy=False)
 
     def __init__(self, sequencing_technology=None):
         super().__init__()
@@ -70,17 +110,27 @@ class Experiment(BaseTable):
         return f"Experiment({self.run!r}, {self.project!r}, {self.sequencing_technology!r}, {self.deleted!r}, {self.extra_metadata!r})"
 
 class Sample(BaseTable):
-    """docstring for Sample"""
+    """
+    Sample:
+        id integer [PK]
+        patient_id integer [ref: > patient.id]
+        experiment_id integer [ref: > experiment.id]
+        name text
+        tumour boolean
+        alias blob
+        deleted boolean
+        extra_metadata json
+    """
     __tablename__ = "sample"
 
     patient_id = Column(Integer, ForeignKey("patient.id"))
     experiment_id = Column(Integer, ForeignKey("experiment.id"))
-    name = Column(String, nullable=False, unique=True)
+    name = Column(String, nullable=False)
     tumour = Column(Boolean, default=False)
     alias = Column(String, nullable=True)
 
-    patient = relation("Patient", backref="sample", lazy=False)
-    experiment = relation("Patient", backref="sample", lazy=False)
+    patient = relationship("Patient", backref="sample", lazy=False)
+    experiment = relationship("Experiment", backref="sample", lazy=False)
 
     def __init__(self, name=None, tumour=None, alias=None):
         super().__init__()
@@ -92,7 +142,15 @@ class Sample(BaseTable):
         return f"Sample({self.patient!r}, {self.experiment!r}, {self.name!r}, {self.tumour!r}, {self.alias!r}, {self.deleted!r}, {self.extra_metadata!r})"
 
 class Run(BaseTable):
-    """docstring for Patient"""
+    """
+    Patient:
+        id integer [PK]
+        lab_id text
+        name text
+        date timestamp
+        deleted boolean
+        extra_metadata json
+    """
     __tablename__ = "run"
 
     lab_id = Column(String, nullable=True)
@@ -109,7 +167,21 @@ class Run(BaseTable):
         return f"Run({self.lab_id!r}, {self.name!r}, {self.date!r}, {self.deleted!r}, {self.extra_metadata!r})"
 
 class Readset(BaseTable):
-    """docstring for Readset"""
+    """
+    Readset:
+        id integer [PK]
+        sample_id integer [ref: > sample.id]
+        run_id integer [ref: > run.id]
+        name text
+        lane text
+        adapter1 text
+        adapter2 text
+        sequencing_type text
+        quality_offset text
+        alias blob
+        deleted boolean
+        extra_metadata json
+    """
     __tablename__ = "readset"
 
     sample_id = Column(Integer, ForeignKey("sample.id"))
@@ -122,8 +194,8 @@ class Readset(BaseTable):
     quality_offset = Column(String, nullable=True)
     alias = Column(String, nullable=True)
 
-    sample = relation("Sample", backref="readset", lazy=False)
-    run = relation("Run", backref="readset", lazy=False)
+    sample = relationship("Sample", backref="readset", lazy=False)
+    run = relationship("Run", backref="readset", lazy=False)
 
     def __init__(self, name=None, lane=None, adapter1=None, adapter2=None, sequencing_type=None, quality_offset=None, alias=None):
         super().__init__()
@@ -139,7 +211,16 @@ class Readset(BaseTable):
         return f"Readset({self.sample!r}, {self.run!r}, {self.name!r}, {self.lane!r}, {self.adapter1!r}, {self.adapter2!r}, {self.sequencing_type!r}, {self.quality_offset!r}, {self.alias!r}, {self.deleted!r}, {self.extra_metadata!r})"
 
 class Step(BaseTable):
-    """docstring for Step"""
+    """
+    Step:
+        id integer [PK]
+        sample_id integer [ref: > sample.id]
+        readset_id integer [ref: > readset.id]
+        name text
+        status text
+        deleted boolean
+        extra_metadata json
+    """
     __tablename__ = "step"
 
     sample_id = Column(Integer, ForeignKey("sample.id"))
@@ -147,8 +228,8 @@ class Step(BaseTable):
     name = Column(String, nullable=False)
     status = Column(String, nullable=True)
 
-    sample = relation("Sample", backref="step", lazy=False)
-    readset = relation("Readset", backref="step", lazy=False)
+    sample = relationship("Sample", backref="step", lazy=False)
+    readset = relationship("Readset", backref="step", lazy=False)
 
     def __init__(self, name=None, status=None):
         super().__init__()
@@ -159,7 +240,18 @@ class Step(BaseTable):
         return f"Step({self.sample!r}, {self.readset!r}, {self.name!r}, {self.status!r}, {self.deleted!r}, {self.extra_metadata!r})"
 
 class Job(BaseTable):
-    """docstring for Job"""
+    """
+    Job:
+        id integer [PK]
+        step_id integer [ref: > step.id]
+        name text
+        start timestamp
+        stop timestamp
+        status text
+        type text
+        deleted boolean
+        extra_metadata json
+    """
     __tablename__ = "job"
 
     step_id = Column(Integer, ForeignKey("step.id"))
@@ -169,7 +261,7 @@ class Job(BaseTable):
     status = Column(String, nullable=True)
     type = Column(String, nullable=True)
 
-    step = relation("Step", backref="job", lazy=False)
+    step = relationship("Step", backref="job", lazy=False)
 
     def __init__(self, name=None, start=None, stop=None, status=None, type=None):
         super().__init__()
@@ -183,7 +275,16 @@ class Job(BaseTable):
         return f"Job({self.step!r}, {self.name!r}, {self.start!r}, {self.stop!r}, {self.status!r}, {self.type!r}, {self.deleted!r}, {self.extra_metadata!r})"
 
 class Metric(BaseTable):
-    """docstring for Metric"""
+    """
+    Metric:
+        id integer [PK]
+        job_id integer [ref: > job.id]
+        name text
+        value text
+        flag text //pass, warn, fail
+        deleted boolean
+        extra_metadata json
+    """
     __tablename__ = "metric"
 
     job_id = Column(Integer, ForeignKey("job.id"))
@@ -191,7 +292,7 @@ class Metric(BaseTable):
     value = Column(String, nullable=True)
     flag = Column(String, nullable=True)
 
-    job = relation("Job", backref="metric", lazy=False)
+    job = relationship("Job", backref="metric", lazy=False)
 
     def __init__(self, name=None, value=None, flag=None):
         super().__init__()
@@ -203,7 +304,17 @@ class Metric(BaseTable):
         return f"Metric({self.job!r}, {self.name!r}, {self.value!r}, {self.flag!r}, {self.deleted!r}, {self.extra_metadata!r})"
 
 class File(BaseTable):
-    """docstring for File"""
+    """
+    File:
+        id integer [PK]
+        job_id integer [ref: > job.id]
+        path text
+        type text
+        description text
+        creation timestamp
+        deleted boolean
+        extra_metadata json
+    """
     __tablename__ = "file"
 
     job_id = Column(Integer, ForeignKey("job.id"))
@@ -212,7 +323,7 @@ class File(BaseTable):
     description = Column(String, nullable=True)
     creation = Column(DateTime, nullable=True)
 
-    job = relation("Job", backref="file", lazy=False)
+    job = relationship("Job", backref="file", lazy=False)
 
     def __init__(self, path=None, type=None, description=None, creation=None):
         super().__init__()
@@ -223,6 +334,28 @@ class File(BaseTable):
 
     def __repr__(self):
         return f"File({self.job!r}, {self.path!r}, {self.type!r}, {self.description!r}, {self.creation!r}, {self.deleted!r}, {self.extra_metadata!r})"
+
+def insert(engine, entry, **relations):
+    """
+    entry: Table object ex. Movie("Robocop", 1987)
+    **relations: name = value ex. director = Director("Paul Verhoeven")
+    """
+    local_session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+    # With this we get a session to do whatever we want to do
+    session = local_session()
+
+    # new_entry = entry
+    for name, relation_entry in relations.items():
+        session.get(relation_entry)
+        setattr(entry, name, relation_entry)
+        # new_entry.name = relation_entry
+
+    try:
+        session.add(entry)
+        session.commit()
+    except Exception as error:
+        print(f"Error: {error}")
+        session.rollback()
 
 # Cf. https://stackoverflow.com/questions/48722835/custom-type-hint-annotation
 # T = typing.TypeVar('T')
