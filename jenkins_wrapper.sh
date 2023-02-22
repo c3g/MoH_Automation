bee25de7a094993f809db1ae61ba1c98f48786ca
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2023-02-22 16:29:14 -0500

    jenkins wrapper verbosity

--- jenkins_wrapper.sh
+++ jenkins_wrapper.sh
@@ -7,7 +7,7 @@ module load mugqic/python/3.10.4
 
 ##################################################
 # STEP 1: Update file paths and timestamps in DB
-echo "Running MOH_Check_Progress.py ..."
+echo "-> Running MOH_Check_Progress.py..."
 ./MOH_Check_Progress.py
 echo -e "\n"
 
@@ -16,7 +16,7 @@ sleep 30
 
 ##################################################
 # STEP 2: Update metrics in DB
-echo "Running Metrics_Update.py ..."
+echo "-> Running Metrics_Update.py..."
 ./Metrics_Update.py
 echo -e "\n"
 
@@ -25,9 +25,9 @@ sleep 30
 
 ##################################################
 # STEP 3: Update CSV files after DB modification
-echo "Running Create_CSVs.sh ..."
+echo "-> Running Create_CSVs.sh..."
 ./Create_CSVs.sh
-echo -e "Done.\n"
+echo -e "...Done.\n"
 
 # Make sure previous job is done interacting with db
 # sleep 30
