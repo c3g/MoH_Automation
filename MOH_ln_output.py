88584e67e8801e28b54fa41fe11896b0dd5e1e06
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2023-07-03 15:06:04 -0400

    Debug topup path

--- MOH_ln_output.py
+++ MOH_ln_output.py
@@ -273,14 +273,14 @@ def deliver_dna(
     # check if topup
     for bam_n in glob.glob(os.path.join(RAW_READS_FOLDER, f"{patient.sample}-*DN")):
         if bam_n != beluga_bam_dna_n:
-            updated = get_link_log(bam_n, raw_folder, bam_n, log, updated, old_log)
+            updated = get_link_log(bam_n, raw_folder, os.path.basename(bam_n), log, updated, old_log)
     updated = get_link_log(beluga_bam_dna_n, raw_folder, f"{patient.dna_n}.bam", log, updated, old_log)
 
     beluga_bam_dna_t = extract_fileloc_field(connection, patient.sample, "Beluga_BAM_DNA_T")
     # check if topup
     for bam_t in glob.glob(os.path.join(RAW_READS_FOLDER, f"{patient.sample}-*DT")):
         if bam_t != beluga_bam_dna_n:
-            updated = get_link_log(bam_t, raw_folder, bam_t, log, updated, old_log)
+            updated = get_link_log(bam_t, raw_folder, os.path.basename(bam_t), log, updated, old_log)
     updated = get_link_log(beluga_bam_dna_t, raw_folder, f"{patient.dna_t}.bam", log, updated, old_log)
 
     os.makedirs(var_folder, exist_ok=True)
