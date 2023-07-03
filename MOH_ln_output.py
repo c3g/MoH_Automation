d11b06789bd499bbc319b8a314015c51d5a0493c
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2023-07-03 15:14:44 -0400

    Debug

--- MOH_ln_output.py
+++ MOH_ln_output.py
@@ -271,14 +271,14 @@ def deliver_dna(
     os.makedirs(raw_folder, exist_ok=True)
     beluga_bam_dna_n = extract_fileloc_field(connection, patient.sample, "Beluga_BAM_DNA_N")
     # check if topup
-    for bam_n in glob.glob(os.path.join(RAW_READS_FOLDER, f"{patient.sample}-*DN")):
+    for bam_n in glob.glob(os.path.join(RAW_READS_FOLDER, f"{patient.sample}-*DN", "*.bam")):
         if bam_n != beluga_bam_dna_n:
             updated = get_link_log(bam_n, raw_folder, os.path.basename(bam_n), log, updated, old_log)
     updated = get_link_log(beluga_bam_dna_n, raw_folder, f"{patient.dna_n}.bam", log, updated, old_log)
 
     beluga_bam_dna_t = extract_fileloc_field(connection, patient.sample, "Beluga_BAM_DNA_T")
     # check if topup
-    for bam_t in glob.glob(os.path.join(RAW_READS_FOLDER, f"{patient.sample}-*DT")):
+    for bam_t in glob.glob(os.path.join(RAW_READS_FOLDER, f"{patient.sample}-*DT", "*.bam")):
         if bam_t != beluga_bam_dna_n:
             updated = get_link_log(bam_t, raw_folder, os.path.basename(bam_t), log, updated, old_log)
     updated = get_link_log(beluga_bam_dna_t, raw_folder, f"{patient.dna_t}.bam", log, updated, old_log)
