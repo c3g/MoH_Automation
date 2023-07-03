e8f1cb2f782143e1d422edaf0d3845250c83cd99
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2023-07-03 14:54:03 -0400

    Debug

--- MOH_ln_output.py
+++ MOH_ln_output.py
@@ -272,14 +272,14 @@ def deliver_dna(
     beluga_bam_dna_n = extract_fileloc_field(connection, patient.sample, "Beluga_BAM_DNA_N")
     # check if topup
     for beluga_bams_dna_n in glob.glob(os.path.join(RAW_READS_FOLDER, f"{patient.sample}-*DN")):
-        if bam not beluga_bam_dna_n:
+        if bam != beluga_bam_dna_n:
             updated = get_link_log(bam, raw_folder, bam, log, updated, old_log)
     updated = get_link_log(beluga_bam_dna_n, raw_folder, f"{patient.dna_n}.bam", log, updated, old_log)
 
     beluga_bam_dna_t = extract_fileloc_field(connection, patient.sample, "Beluga_BAM_DNA_T")
     # check if topup
     for beluga_bam_dna_t in glob.glob(os.path.join(RAW_READS_FOLDER, f"{patient.sample}-*DT")):
-        if bam not beluga_bam_dna_n:
+        if bam != beluga_bam_dna_n:
             updated = get_link_log(bam, raw_folder, bam, log, updated, old_log)
     updated = get_link_log(beluga_bam_dna_t, raw_folder, f"{patient.dna_t}.bam", log, updated, old_log)
 
