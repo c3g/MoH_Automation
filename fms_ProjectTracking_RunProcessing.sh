6914d5e13d222b36a9af692a7b837484e034711b
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2024-12-17 16:33:13 -0500

    Adding a script to transfer fms RP and calling it with nohup via Jenkins to avoid blocking Jenkins during the transfer

--- fms_ProjectTracking_RunProcessing.sh
+++ fms_ProjectTracking_RunProcessing.sh
@@ -40,32 +40,6 @@ while getopts 'hr:d::l::s::x:' OPTION; do
   esac
 done
 
-# location on Beluga. CURRENTLY VERY IMPORTANT. DO NOT CHANGE OR IT WILL BREAK THE DATABASE
-# SERIOUSLY DON'T CHANGE IT.
-# PLEASE DONT.
-if [[ $destination = Beluga ]]; then
-    # Beluga main folder location
-    DEST_LOC="/lustre03/project/6007512/C3G/projects/MOH_PROCESSING/raw_reads"
-    # Beluga log file location
-    DEST_LOG_LOC="/lustre03/project/6007512/C3G/projects/MOH_PROCESSING/DATABASE/log_files/transfer"
-    # Beluga Endpoint
-    DEST_EP='278b9bfe-24da-11e9-9fa2-0a06afd4a22e'
-elif [[ $destination = Cardinal ]]; then
-    # Cardinal main folder location
-    DEST_LOC="/project/60007/MOH/MAIN/raw_reads"
-    # Cardinal log file location
-    DEST_LOG_LOC="/project/60007/MOH/log_files/transfer"
-    # Cardinal Endpoint
-    DEST_EP='26f926d9-6216-4e84-9037-a5c9567b5707'
-elif [[ $destination = Abacus ]]; then
-    # Abacus main folder location
-    DEST_LOC="/lb/project/mugqic/projects/MOH/MAIN/raw_reads"
-    # Abacus log file location
-    DEST_LOG_LOC="/lb/project/mugqic/projects/MOH/log_files/transfer"
-    # Abacus Endpoint
-    DEST_EP=''
-fi
-
 run_processing2json_args=""
 if declare -p lane >&/dev/null; then
   run_processing2json_args="${run_processing2json_args} -l ${lane[*]}"
@@ -97,14 +71,6 @@ cd "$path"
 folder_prefix=/lb/robot/research/freezeman-processing
 input=$(find "$folder_prefix"/*/*/ -type d -name "$runfolder")
 echo "-> Processing $runfolder..."
-# Temporary File location, you may want to change it to your scratch for easier clean up.
-TEMP='/lb/project/mugqic/projects/MOH/TEMP'
-TIMESTAMP=$(date +%FT%H.%M.%S)
-LOGFILE="${runfolder}_${TIMESTAMP}_transfer.log"
-LISTFILE="${runfolder}_${TIMESTAMP}_transfer.list"
-
-# abacus Endpoint
-ABA_EP='26261fd6-0e6d-4252-a0ea-410b4b4f2eef'
 
 if [ -s "$input" ]; then
   run_processing_json=$path/$runfolder.json
@@ -124,45 +90,5 @@ else
   exit 1
 fi
 
-touch "$TEMP/$LOGFILE"
-touch "$TEMP/$LISTFILE"
-echo "Log file of transfer from Abacus to $destination" > "$TEMP/$LOGFILE"
-echo "Transferred From $input" >> "$TEMP/$LOGFILE"
-
-for sample_name in $(jq -r '.. | .sample_name? // empty' $run_processing_json); do
-  for file in $(jq -r --arg sample_name "$sample_name" '.. | select(.sample_name? == $sample_name) | .readset[]?.file[]?.location_uri? // empty | sub("^abacus://"; "")' $run_processing_json); do
-    echo "$file $DEST_LOC/$sample_name/$(basename $file)" >> "$TEMP/$LISTFILE"
-    echo "$file,$sample_name/$(basename $file)" >> "$TEMP/$LOGFILE"
-  done
-done
-
-echo "$TEMP/$LOGFILE $DEST_LOG_LOC/$LOGFILE" >> "$TEMP/$LISTFILE"
-
-if [[ $destination != Abacus ]]; then
-  # Load globus module
-  module load mugqic/globus-cli/3.24.0
-
-  # Generate and store a UUID for the submission-id
-  sub_id="$(globus task generate-submission-id)"
-
-  # Start the batch transfer
-  task_id="$(globus transfer --sync-level mtime --jmespath 'task_id' --format=UNIX --submission-id "$sub_id" --label "$runfolder" --batch "$TEMP/$LISTFILE" $ABA_EP $DEST_EP)"
-
-  echo "Waiting on 'globus transfer' task '$task_id'"
-  globus task wait "$task_id" --polling-interval 60 -H
-  # shellcheck disable=SC2181
-  if [ $? -eq 0 ]; then
-    module unload mugqic/globus-cli/3.24.0
-    # shellcheck disable=SC2086
-    ~/moh_automation/transfer2json.py --input $TEMP/$LISTFILE --source "abacus" --destination $destination --output /lb/project/mugqic/projects/MOH/Transfer_json/${LISTFILE/.list/.json} --operation_cmd_line "globus transfer --sync-level mtime --jmespath 'task_id' --format=UNIX --submission-id $sub_id --label $runfolder --batch $TEMP/$LISTFILE $ABA_EP $DEST_EP"
-    # shellcheck disable=SC2086
-    pt-cli ingest transfer --input-json /lb/project/mugqic/projects/MOH/Transfer_json/${LISTFILE/.list/.json}
-  else
-    echo "$task_id failed!"
-  fi
-else
-  while IFS= read -r line; do
-    mkdir -p $(dirname $(echo $line | awk '{print $2}'))
-    ln -sf $line
-  done < "$TEMP/$LISTFILE"
-fi
+nohup ~/fms_transfer_RunProcessing.sh -r "$run_processing_json" -d "$destination" &
+echo "Transfer started."
---
+++ fms_transfer_RunProcessing.sh
@@ -0,0 +1,136 @@
+#!/usr/bin/env bash
+
+THIS_SCRIPT=$(basename "$0")
+
+usage() {
+  echo "script usage: $THIS_SCRIPT -h [-l run_processing_json] [-d destination] [-s sample]"
+  echo "Usage:"
+  echo " -h                               Display this help message."
+  echo " -r <run_processing_json>         Run Processing json."
+  echo " -d <destination>                 Destination for the transfer (either Beluga or Cardinal or Abacus)."
+  exit 1
+  }
+
+while getopts 'hr:d:' OPTION; do
+  case "$OPTION" in
+    r)
+      run_processing_json="$OPTARG"
+      ;;
+    d)
+      destination="$OPTARG"
+      ;;
+    h)
+      usage
+      ;;
+    ?)
+      usage
+      ;;
+  esac
+done
+
+# mandatory arguments
+if [ ! "$run_processing_json" ] || [ ! "$destination" ]; then
+  echo -e "ERROR: Missing mandatory argument -r and/or -d.\n"
+  usage
+fi
+
+if ! [[ $destination =~ Cardinal|Beluga|Abacus ]]; then
+    echo -e "ERROR: Invalid destination: '$destination'. It has to be either Beluga, Cardinal or Abacus.\n"
+    usage
+fi
+
+# location on Beluga. CURRENTLY VERY IMPORTANT. DO NOT CHANGE OR IT WILL BREAK THE DATABASE
+# SERIOUSLY DON'T CHANGE IT.
+# PLEASE DONT.
+if [[ $destination = Beluga ]]; then
+    # Beluga Endpoint
+    DEST_EP='278b9bfe-24da-11e9-9fa2-0a06afd4a22e'
+    # Beluga main folder location
+    DEST_LOC="/lustre03/project/6007512/C3G/projects/MOH_PROCESSING/raw_reads"
+    # Beluga log file location
+    DEST_LOG_LOC="/lustre03/project/6007512/C3G/projects/MOH_PROCESSING/DATABASE/log_files/transfer"
+elif [[ $destination = Cardinal ]]; then
+    # Cardinal Endpoint
+    DEST_EP='26f926d9-6216-4e84-9037-a5c9567b5707'
+    # Cardinal main folder location
+    DEST_LOC="/project/60007/MOH/MAIN/raw_reads"
+    # Cardinal log file location
+    DEST_LOG_LOC="/project/60007/MOH/log_files/transfer"
+elif [[ $destination = Abacus ]]; then
+    # Abacus Endpoint
+    DEST_EP=''
+    # Abacus main folder location
+    DEST_LOC="/lb/project/mugqic/projects/MOH/MAIN/raw_reads"
+    # Abacus log file location
+    DEST_LOG_LOC="/lb/project/mugqic/projects/MOH/log_files/transfer"
+fi
+
+# abacus Endpoint
+ABA_EP='26261fd6-0e6d-4252-a0ea-410b4b4f2eef'
+
+runfolder=$(jq -r '.run_name' "$run_processing_json")
+
+# Temporary File location, you may want to change it to your scratch for easier clean up.
+TEMP='/lb/project/mugqic/projects/MOH/TEMP'
+TIMESTAMP=$(date +%FT%H.%M.%S)
+LOGFILE="${runfolder}_${TIMESTAMP}_transfer.log"
+LISTFILE="${runfolder}_${TIMESTAMP}_transfer.list"
+
+touch "$TEMP/$LOGFILE"
+touch "$TEMP/$LISTFILE"
+echo "Log file of transfer from Abacus to $destination" > "$TEMP/$LOGFILE"
+
+jq -r '
+  .specimen[]?.sample[]? | 
+  .sample_name as $sample_name | 
+  .readset[]? | 
+  .readset_lane as $readset_lane | 
+  .file[]? | 
+  select(.location_uri != null) | 
+  "\($sample_name) \($readset_lane) \(.location_uri | sub("^abacus://"; ""))"
+' "$run_processing_json" | while read -r sample_name readset_lane file; do
+  file_basename=$(basename "$file")
+  file_without_extension="${file_basename%%.sorted.*}"
+  file_extension="${file_basename##*.}"
+  new_filename="${file_without_extension}_L00${readset_lane}.sorted.${file_extension}"
+  if [[ ($file == *.bam || $file == *.bai) && "$file_basename" != "$new_filename" ]]; then
+    echo "$file $DEST_LOC/$sample_name/$new_filename" >> "$TEMP/$LISTFILE"
+    echo "$file,$sample_name/$new_filename" >> "$TEMP/$LOGFILE"
+  else
+    echo "$file $DEST_LOC/$sample_name/$file_basename" >> "$TEMP/$LISTFILE"
+    echo "$file,$sample_name/$file_basename" >> "$TEMP/$LOGFILE"
+  fi
+done
+
+echo "$TEMP/$LOGFILE $DEST_LOG_LOC/$LOGFILE" >> "$TEMP/$LISTFILE"
+
+if [[ $destination != Abacus ]]; then
+  # Load globus module
+  module load mugqic/globus-cli/3.24.0
+
+  # Generate and store a UUID for the submission-id
+  sub_id="$(globus task generate-submission-id)"
+  # Start the batch transfer
+  # shellcheck disable=SC2086
+  task_id="$(globus transfer --sync-level mtime --jmespath 'task_id' --format=UNIX --submission-id "$sub_id" --label "$runfolder" --batch "$TEMP/$LISTFILE" $ABA_EP $DEST_EP)"
+
+  echo "Waiting on 'globus transfer' task '$task_id'"
+  globus task wait "$task_id" --polling-interval 60 -H
+  # shellcheck disable=SC2181
+  if [ $? -eq 0 ]; then
+    module unload mugqic/globus-cli/3.24.0
+    # shellcheck disable=SC2086
+    ~/moh_automation/transfer2json.py --input $TEMP/$LISTFILE --source "abacus" --destination $destination --output /lb/project/mugqic/projects/MOH/Transfer_json/${LISTFILE/.list/.json} --operation_cmd_line "globus transfer --sync-level mtime --jmespath 'task_id' --format=UNIX --submission-id $sub_id --label $runfolder --batch $TEMP/$LISTFILE $ABA_EP $DEST_EP"
+    # shellcheck disable=SC2086
+    pt-cli ingest transfer --input-json /lb/project/mugqic/projects/MOH/Transfer_json/${LISTFILE/.list/.json}
+  else
+    echo "$task_id failed!"
+  fi
+else
+  while IFS= read -r line; do
+    # shellcheck disable=SC2046,SC2086
+    mkdir -p $(dirname $(echo $line | awk '{print $2}'))
+    # shellcheck disable=SC2086
+    ln -sf $line
+  done < "$TEMP/$LISTFILE"
+fi
