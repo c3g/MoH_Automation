985410140e2a1b17df183432f29a2c339d28e1eb
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2025-09-29 16:07:16 -0400

    Using non user account to do transfers for raw data

--- fms_transfer_RunProcessing.sh
+++ fms_transfer_RunProcessing.sh
@@ -39,24 +39,30 @@ if ! [[ $destination =~ Cardinal|Rorqual|Abacus ]]; then
     usage
 fi
 
+ROBOT_EP="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/globus_collections.json"
+
+destination_lowercase="${destination,,}"
+DEST_EP=$(jq -r --arg dest "$destination_lowercase" '.robot_endpoints[$dest].uuid' "$ROBOT_EP")
+DEST_BASE_PATH=$(jq -r --arg dest "$destination_lowercase" '.robot_endpoints[$dest].base_path' "$ROBOT_EP")
+
 if [[ $destination = Rorqual ]]; then
     # Rorqual Endpoint
-    DEST_EP='f19f13f5-5553-40e3-ba30-6c151b9d35d4'
+    # DEST_EP='f19f13f5-5553-40e3-ba30-6c151b9d35d4'
     # Rorqual_MoH_Robot Endpoint
     # DEST_EP='b8ae5665-590d-45b9-ae83-d1dfde08e7d0'
     # Rorqual base path
-    DEST_BASE_PATH="/project/6007512/C3G/projects/MOH_PROCESSING/MAIN"
+    # DEST_BASE_PATH="/project/6007512/C3G/projects/MOH_PROCESSING/MAIN"
     # Rorqual main folder location
     DEST_LOC="${DEST_BASE_PATH}/MAIN/raw_reads"
     # Rorqual log file location
     DEST_LOG_LOC="${DEST_BASE_PATH}/DATABASE/log_files/transfer"
 elif [[ $destination = Cardinal ]]; then
     # Cardinal Endpoint
-    DEST_EP='a6df16bc-4e7f-4784-9afa-8ceb7b20b7c0'
+    # DEST_EP='a6df16bc-4e7f-4784-9afa-8ceb7b20b7c0'
     # Cardinal_MoH_Robot Endpoint
     # DEST_EP='995863d1-97fe-4af2-bc48-c9bf91c33f08'
     # Cardinal base path
-    DEST_BASE_PATH="/project/60007/MOH"
+    # DEST_BASE_PATH="/project/60007/MOH"
     # Cardinal main folder location
     DEST_LOC="${DEST_BASE_PATH}/MAIN/raw_reads"
     # Cardinal log file location
@@ -70,16 +76,21 @@ elif [[ $destination = Abacus ]]; then
     DEST_LOG_LOC="/lb/project/mugqic/projects/MOH/log_files/transfer"
 fi
 
-# abacus Endpoint
-ABA_EP='26261fd6-0e6d-4252-a0ea-410b4b4f2eef'
+# Abacus Endpoint
+# ABA_EP='26261fd6-0e6d-4252-a0ea-410b4b4f2eef'
+# Abacus_RawData_freezeman-processing_Robot
+ABA_EP='fb9e89bd-7bfb-4842-9c4d-9190eb946160'
+SRC_BASE_PATH="/lb/robot/research/freezeman-processing"
 
 runfolder=$(jq -r '.run_name' "$run_processing_json")
 
 # Temporary File location, you may want to change it to your scratch for easier clean up.
+TRANSFER_JSON_DIR='/lb/project/mugqic/projects/MOH/Transfer_json'
 TEMP='/lb/project/mugqic/projects/MOH/TEMP'
 TIMESTAMP=$(date +%FT%H.%M.%S)
 LOGFILE="${runfolder}_${TIMESTAMP}_${destination}_transfer.log"
 LISTFILE="${runfolder}_${TIMESTAMP}_${destination}_transfer.list"
+LISTFILE_FULLPATH="${runfolder}_${TIMESTAMP}_${destination}_transfer_fullpath.list"
 
 touch "$TEMP/$LOGFILE"
 touch "$TEMP/$LISTFILE"
@@ -99,10 +110,12 @@ jq -r '
   file_extension="${file_basename##*.}"
   new_filename="${file_without_extension}_L00${readset_lane}.sorted.${file_extension}"
   if [[ ($file == *.bam || $file == *.bai) && "$file" != *"_L00${readset_lane}.sorted.${file_extension}" ]]; then
-    echo "$file $DEST_LOC/$sample_name/$new_filename" >> "$TEMP/$LISTFILE"
+    echo "${file#"$SRC_BASE_PATH"} ${DEST_LOC#"$DEST_BASE_PATH"}/$sample_name/$new_filename" >> "$TEMP/$LISTFILE"
+    echo "$file $DEST_LOC/$sample_name/$new_filename" >> "$TEMP/$LISTFILE_FULLPATH"
     echo "$file,$sample_name/$new_filename" >> "$TEMP/$LOGFILE"
   else
-    echo "$file $DEST_LOC/$sample_name/$file_basename" >> "$TEMP/$LISTFILE"
+    echo "${file#"$SRC_BASE_PATH"} ${DEST_LOC#"$DEST_BASE_PATH"}/$sample_name/$file_basename" >> "$TEMP/$LISTFILE"
+    echo "$file $DEST_LOC/$sample_name/$file_basename" >> "$TEMP/$LISTFILE_FULLPATH"
     echo "$file,$sample_name/$file_basename" >> "$TEMP/$LOGFILE"
   fi
 done
@@ -112,6 +125,10 @@ echo "$TEMP/$LOGFILE $DEST_LOG_LOC/$LOGFILE" >> "$TEMP/$LISTFILE"
 if [[ $destination != Abacus ]]; then
   # Load globus module
   module load mugqic/globus-cli/3.24.0
+
+  # Now using Robot account to do the transfer
+  # shellcheck disable=SC1090
+  source ~/.config/globus_cli/globus_cli_env.sh
 
   # Generate and store a UUID for the submission-id
   sub_id="$(globus task generate-submission-id)"
@@ -123,12 +140,13 @@ if [[ $destination != Abacus ]]; then
   globus task wait "$task_id" --polling-interval 60 -H
   # shellcheck disable=SC2181
   if [ $? -eq 0 ]; then
+    TRANSFER_JSON="$TRANSFER_JSON_DIR/${LISTFILE/.list/.json}"
     module unload mugqic/globus-cli/3.24.0
     # shellcheck disable=SC2086
-    ~/moh_automation/transfer2json.py --input $TEMP/$LISTFILE --source "abacus" --destination $destination --output /lb/project/mugqic/projects/MOH/Transfer_json/${LISTFILE/.list/.json} --operation_cmd_line "globus transfer --sync-level mtime --jmespath 'task_id' --format=UNIX --submission-id $sub_id --label $runfolder --batch $TEMP/$LISTFILE $ABA_EP $DEST_EP"
-    echo "Ingesting transfer /lb/project/mugqic/projects/MOH/Transfer_json/${LISTFILE/.list/.json}..."
+    ~/moh_automation/transfer2json.py --input $TEMP/$LISTFILE_FULLPATH --source "abacus" --destination $destination --output $TRANSFER_JSON --operation_cmd_line "globus transfer --sync-level mtime --jmespath 'task_id' --format=UNIX --submission-id $sub_id --label $runfolder --batch $TEMP/$LISTFILE $ABA_EP $DEST_EP"
+    echo "Ingesting transfer $TRANSFER_JSON..."
     # shellcheck disable=SC2086
-    pt-cli ingest transfer --input-json /lb/project/mugqic/projects/MOH/Transfer_json/${LISTFILE/.list/.json}
+    pt-cli ingest transfer --input-json $TRANSFER_JSON
   else
     echo "$task_id failed!"
   fi
